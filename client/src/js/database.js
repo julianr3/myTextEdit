fd918f4546552aa55be0276104eef27eabef506b
Julian Richardson <jrichar3@yahoo.com>
2022-05-26 16:07:15 -0400

    added indexDB put function

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,19 +1,28 @@
+import req from 'express/lib/request';
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB('myTextEdit', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains('myTextEdit')) {
+        console.log('myTextEdit database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore('myTextEdit', { keyPath: 'id', autoIncrement: true });
+      console.log('myTextEdit database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  // OPen indexDB
+  const indexDB = await openDB("myTextEdit", 1)
+  const dbtx = indexDB.transaction("myTextEdit", "readwrite")
+  const store = dbtx.objectStore("myTextEdit")
+  const request = store.put({id:1, jate:content})
+  const result = await request;
+  console.log(result)
+}
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
