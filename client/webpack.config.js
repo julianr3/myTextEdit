cc420f65f0641d973fe38fee8a4a2b824ece9935
Julian Richardson <jrichar3@yahoo.com>
2022-05-26 15:55:48 -0400

    made offline functionality

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -19,6 +19,8 @@ module.exports = () => {
     },
     plugins: [
       new WebpackPwaManifest({
+        inject: true,
+        fingerprints: false,
         name: 'myTextEdit',
         short_name: 'myTextEdit',
         description: 'Cool text editor',
